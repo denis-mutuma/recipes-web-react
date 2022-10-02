0fa79804579ca9b93348c76f16c8ffb72dbe928b
Denis Mutuma <mutuma212@gmail.com>
2022-10-02 11:14:33 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain='AUTH0_DOMAIN'
+      clientId='AUTH_CLIENT_ID'
+      redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
